6ddf151c2846eaa7596f5c1295b8b2c30ed28af3
arielc12 <arielcehana1234@gmail.com>
2024-09-24 10:38:49 +0300

    conncetToAtlasDB

--- DB/mongodb/connectToAtlas.js
+++ DB/mongodb/connectToAtlas.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
-const conncectionStringForAtlas = "";
+require("dotenv").config();
+
+const conncectionStringForAtlas = process.env.ATLAS_CONNECTION_STRING;
 const connectToAtlasDb = async () => {
     try {
-        //await mongoose.connect(conncectionStringForAtlas);
+        await mongoose.connect(conncectionStringForAtlas);
         console.log("Connected to MongoDB in atlas");
     } catch (error) {
         console.error("Could not connect to MongoDB", error);
